8822c58edbe863b0138375447d7d03930661ee1b
unknown <nazih.developer@gmail.com>
2022-09-27 20:10:09 +0200

    My first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Mellanrum
+
+//Mellanrum
+
+//Uppgift 1
+function max(number1: number, number2: number): number {
+  return number1 > number2 ? number1 : number2;
+}
+
+//Upgift 2
+function landscape(height: number, width: number): boolean {
+  return width > height;
+}
+
+//Uppgift 3
+function rectangelArea(length: number, width: number): number {
+  return length * width;
+}
+
+//Uppgift 3 Extra
+function triangleArea(heigth: number, width: number): number {
+  return (heigth * width) / 2;
+}
+
+function circleArea(radius: number): number {
+  return radius ** 2 * Math.PI;
+}
+
+// uppgift 4
+function tellFortune(
+  numberOfChildren: number,
+  partner: string,
+  city: string,
+  job: string
+): string {
+  return `You will be an ${job} in the ${city}, married to ${partner} and have ${numberOfChildren} children`;
+}
+
+//uppgift 5
+function calculateDogAge(humanAge: number): number {
+  return humanAge * 7;
+}
+
+//Uppgift 5 Extra
+function calculateDogAgeDynamic(
+  humanAge: number,
+  conversionFactor: number
+): number {
+  return humanAge * conversionFactor;
+}
+
+//Upggift 6
+function daysOfSuppliers(
+  totalResources: number,
+  resourcesPerDay: number
+): number {
+  return Math.floor(totalResources / resourcesPerDay);
+}
+
+//Uppgift 7
+function toFahrenheit(celsius: number): number {
+  return (celsius * 9) / 5 + 32;
+}
+
+//Upgift 7 extra
+function toCelsius(Fahrenheit: number): number {
+  return ((Fahrenheit - 32) * 5) / 9;
+}
+
+//Uppgift 8
+function ageInSeconds(age: number): number {
+  return Math.round(age * 364 * 24 * 60 * 60);
+}
+
+function area(form: string, width: number, heigth: number): number {
+  if (form === "circle") return (width / 2) ** 2 * Math.PI;
+  if (form === "triangle") return (heigth * width) / 2;
+  return heigth * width;
+}
+
+function checkSpeed(speed: number): string | number {
+  const speedlimit: number = 70;
+  const points: number = Math.floor((speed - speedlimit) / 5);
+
+  if (speed <= speedlimit) return "OK";
+  if (points > 12) return "Indraget Körkort";
+  return points;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>{checkSpeed(71)}</div>;
 }
 
 export default App;
